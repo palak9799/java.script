5caa6103225f63edcaa57d2a18cde512a17ec5f2
palak9799 <palakshrimali300@gmail.com>
2026-05-11 22:08:06 +0530

    loops

---
+++ conditionals.js
@@ -0,0 +1,9 @@
+/*age=21;
+if(age>=28) console.log("adult");
+else console.log("child");
+num=15;
+if(num%2==0) console.log("even");
+else console.log("odd");/*/
+x=1999;
+if(x>=2000 && x<1998) console.log("21st century");
+else console.log("20th century");
