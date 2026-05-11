cc60bea9bcfaed8fc99aa47eb70e526e443e03f3
palak9799 <palakshrimali300@gmail.com>
2026-05-11 21:22:42 +0530

    new data typpe

--- fist.js
+++ fist.js
@@ -1,2 +1,25 @@
-console.log("hello world");
+/*console.log("hello world");
 console.log("palak");
+a = 12;
+console.log(a);
+b=12+3;
+console.log(b);
+age = 21;
+console.log(age + "my age is hai" + "hii");
+const a = "palak";
+var a = 5;
+console.log(a+1);
+var a =7;
+console.log(a);
+let a=100;
+let b=120;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(12**12);/*/
+a=123400
+console.log(++a);
+console.log(a--);
+console.log(a+=1);
