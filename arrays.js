44bca87b32e37418b592fbfe6a64437b2ad0524d
palak9799 <palakshrimali300@gmail.com>
2026-05-12 22:03:31 +0530

    stringsbuiltin.js

---
+++ arrays.js
@@ -0,0 +1,15 @@
+/*/arr = [1, 2, 3, 4, 5];
+console.log(arr[0]);
+n = arr.length;
+arr.push(9);
+console.log(arr);
+arr.pop(2);
+console.log(arr);
+arr.unshift(0);//removr karna
+console.log(arr);
+arr.shift(0); //add karna
+console.log(arr);/*/
+brr =[3,4,5,6,7,8];
+for(const ele of brr){
+    console.log(ele);
+}
