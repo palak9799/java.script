351e6d3a20ae4bb511eb51dec49cfbeecb55e555
palak9799 <palakshrimali300@gmail.com>
2026-05-12 22:21:32 +0530

    s.slice(

---
+++ stringsbuiltin.js
@@ -0,0 +1,8 @@
+/*/let s = "palak";
+console.log(s);
+console.log(s.toUpperCase());
+console.log(s);
+console.log(s.indexOf('k'))/*/
+let s ="palak shrimali";
+console.log(s.slice(0,5));
+console.log(s.slice(1));
